142143d4bc8f349e0d3a9bafa8f3215518853347
Martin Kim Dung-Pham <martinkim.pham@gmail.com>
2021-05-01 15:07:38 +0200

    Initial Commit

---
+++ rorschach-example/rorschach_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  rorschach_exampleApp.swift
+//  rorschach-example
+//
+//  Created by Martin Kim Dung-Pham on 01.05.21.
+//
+
+import SwiftUI
+
+@main
+struct rorschach_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
